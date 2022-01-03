040fb70ac40243137f5e525faa6b7cacbfc2f79e
Sara Alhajri <93153872+ssh218@users.noreply.github.com>
2022-01-03 16:08:28 +0300

    n

---
+++ Sara/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import *
+
+def products(request):
+    productss = Product.objects.all()
+    return render(request, 'products.html', {'productss':productss})
